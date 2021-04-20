d28b6369ad1885b9df431b080b3f3e5fb1a18e75
BlagovestMihaylov <BlagovestMihaylov@abv.bg>
2021-04-20 16:22:47 +0300

    next must be fight and eventually dataLog

--- Homeworks/Homework3/main.cpp
+++ Homeworks/Homework3/main.cpp
@@ -226,122 +226,72 @@ int fight(Player<WaterCard, EarthCard> player1, Player<FireCard, WindCard> playe
 int main()
 {
 
-    // Earth.FullStats();
-    // std::cout << std::endl;
-    // FireCard Fire("Brand", 15, 75);
-    // Fire.addPower(420);
-    // Fire.FullStats();
-    // std::cout << std::endl;
-    // WindCard Wind("Yasuo", 1337, 55);
-    // Wind.FullStats();
-    // std::cout << std::endl;
-    // WaterCard Water("Nami", 0, 1234);
-    // Water.FullStats();
-    // EarthCard Earth("Darius", 10, 69);
-    // Earth.addPower(21);
-    // Earth.changeCardName("Malphite");
-    // EarthCard Earth1("Maplhite1", 10, 69);
-    // EarthCard Earth2("Maplhite2", 10, 69);
-    // EarthCard Earth3("Maplhite3", 10, 69);
-    // EarthCard Earth4("Maplhite1", 10, 69);
-    // Deck<EarthCard> earthDeck;
-    // earthDeck.addCard(Earth);
-    // earthDeck.addCard(Earth1);
-    // earthDeck.addCard(Earth2);
-    // earthDeck.addCard(Earth3);
-    // earthDeck.addCard(Earth4);
-    // earthDeck.ShowDeck();
-    // EarthCard e = earthDeck.drawCard();
-
-    //  std::cout << "Old deck" << std::endl;
-    // earthDeck.ShowDeck();
-
-    // std::cout << std::endl;
-    // std::cout << std::endl;
-    // std::cout << std::endl;
-    // std::cout << "New deck" << std::endl;
-    // earthDeck.shuffle();
-    // earthDeck.ShowDeck();
-
-    // ///////////////////////////////////
-    // WaterCard Water("Riven", 10, 69);
-    // Water.addPower(21);
-    // Water.changeCardName("Nami");
-    // WaterCard Water1("Nami1", 10, 69);
-    // WaterCard Water2("Nami2", 10, 69);
-    // WaterCard Water3("Nami3", 10, 69);
-    // WaterCard Water4("Nami1", 10, 69);
-    // Deck<WaterCard> waterDeck;
-    // waterDeck.addCard(Water);
-    // waterDeck.addCard(Water1);
-    // waterDeck.addCard(Water2);
-    // waterDeck.addCard(Water3);
-    // waterDeck.addCard(Water4);
-    // waterDeck.ShowDeck();
-    // WaterCard w = waterDeck.drawCard();
-
-    // Player<WaterCard, EarthCard> player1;
-    // player1.changePlayerName("Blagovest");
-    // player1.changeLosses(4);
-    // player1.changeWins(4);
-    // player1.addPrimaryDeck(waterDeck);
-    // player1.addSecondaryDeck(earthDeck);
-    // player1.FullStats();
-    // w.FullStats();
-    // e.FullStats();
-    // counter(e, w);
-    // std::cout << std::endl;
-    // std::cout << std::endl;
-    // std::cout << std::endl;
-    // w.FullStats();
-    // e.FullStats();
-
-    // ///////////////////////////////////
-    // WindCard Wind("Riven", 10, 69);
-    // Wind.addPower(21);
-    // Wind.changeCardName("Yasuo");
-    // WindCard Wind1("Yasuo1", 10, 69);
-    // WindCard Wind2("Yasuo2", 10, 69);
-    // WindCard Wind3("Yasuo3", 10, 69);
-    // WindCard Wind4("Yasuo1", 10, 69);
-    // Deck<WindCard> windDeck;
-    // windDeck.addCard(Wind);
-    // windDeck.addCard(Wind1);
-    // windDeck.addCard(Wind2);
-    // windDeck.addCard(Wind3);
-    // windDeck.addCard(Wind4);
-    // WindDeck.ShowDeck();
-    // WindCard wn = WindDeck.drawCard();
-
-    // ///////////////////////////////////
-    // FireCard Fire("Riven", 10, 69);
-    // Fire.addPower(21);
-    // Fire.changeCardName("Brand");
-    // FireCard Fire1("Brand1", 10, 69);
-    // FireCard Fire2("Brand2", 10, 69);
-    // FireCard Fire3("Brand3", 10, 69);
-    // FireCard Fire4("Brand1", 10, 69);
-    // Deck<FireCard> fireDeck;
-    // fireDeck.addCard(Fire);
-    // fireDeck.addCard(Fire1);
-    // fireDeck.addCard(Fire2);
-    // fireDeck.addCard(Fire3);
-    // fireDeck.addCard(Fire4);
-    // FireDeck.ShowDeck();
-    // FireCard f = FireDeck.drawCard();
-    // Player<FireCard, WindCard> player2;
-    // player2.changeLosses(2);
-    // player2.changeWins(3);
-    // player2.changePlayerName("Faker");
-    // player2.addPrimaryDeck(fireDeck);
-    // player2.addSecondaryDeck(windDeck);
-
     Player<WaterCard, EarthCard> player1;
     Player<FireCard, WindCard> player2;
+
     Deck<WaterCard> waterDeck;
+
+    WaterCard Water("Riven", 10, 69);
+    Water.addPower(21);
+    Water.changeCardName("Nami");
+    WaterCard Water1("Nami1", 10, 69);
+    WaterCard Water2("Nami2", 10, 69);
+    WaterCard Water3("Nami3", 10, 69);
+    WaterCard Water4("Nami1", 10, 69);
+
+    waterDeck.addCard(Water);
+    waterDeck.addCard(Water1);
+    waterDeck.addCard(Water2);
+    waterDeck.addCard(Water3);
+    waterDeck.addCard(Water4);
+
     Deck<EarthCard> earthDeck;
+
+    EarthCard Earth("Darius", 10, 69);
+    Earth.addPower(21);
+    Earth.changeCardName("Malphite");
+    EarthCard Earth1("Maplhite1", 10, 69);
+    EarthCard Earth2("Maplhite2", 10, 69);
+    EarthCard Earth3("Maplhite3", 10, 69);
+    EarthCard Earth4("Maplhite1", 10, 69);
+
+    earthDeck.addCard(Earth);
+    earthDeck.addCard(Earth1);
+    earthDeck.addCard(Earth2);
+    earthDeck.addCard(Earth3);
+    earthDeck.addCard(Earth4);
+
     Deck<FireCard> fireDeck;
+
+    FireCard Fire("Riven", 10, 69);
+    Fire.addPower(21);
+    Fire.changeCardName("Brand");
+    FireCard Fire1("Brand1", 10, 69);
+    FireCard Fire2("Brand2", 10, 69);
+    FireCard Fire3("Brand3", 10, 69);
+    FireCard Fire4("Brand1", 10, 69);
+
+    fireDeck.addCard(Fire);
+    fireDeck.addCard(Fire1);
+    fireDeck.addCard(Fire2);
+    fireDeck.addCard(Fire3);
+    fireDeck.addCard(Fire4);
+
     Deck<WindCard> windDeck;
+
+    WindCard Wind("Riven", 10, 69);
+    Wind.addPower(21);
+    Wind.changeCardName("Yasuo");
+    WindCard Wind1("Yasuo1", 10, 69);
+    WindCard Wind2("Yasuo2", 10, 69);
+    WindCard Wind3("Yasuo3", 10, 69);
+    WindCard Wind4("Yasuo1", 10, 69);
+    windDeck.addCard(Wind);
+    windDeck.addCard(Wind1);
+    windDeck.addCard(Wind2);
+    windDeck.addCard(Wind3);
+    windDeck.addCard(Wind4);
+
     player1.addPrimaryDeck(waterDeck);
     player1.addSecondaryDeck(earthDeck);
     player2.addPrimaryDeck(fireDeck);
@@ -352,14 +302,7 @@ int main()
     player2.loadPlayer(in2);
     std::ofstream out("dataLog.txt");
     player1.savePlayer(out);
-    player2.savePlayer(out);
-    // std::ifstream firein("fireDeck.txt");
-    // fireDeck.loadDeck(firein, 5);
-    // player2.addPrimaryDeck(fireDeck);
-    // player2.FullStats();
-    // FireCard fi;
-    // fi.loadCard(in1);
-    // fi.FullStats();
+
+   // player1.changePlayerName("gosho");
     player1.FullStats();
-    std::cout << "HELLLO" << std::endl;
 }
